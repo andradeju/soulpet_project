6f0a373dd65fad0bd4f8bfa695044e3051058749
andradeju <julliana.andrade08@gmail.com>
2023-04-17 22:15:09 -0300

    feat: Listagem de todos os clientes e detalhes

--- index.js
+++ index.js
@@ -13,11 +13,41 @@ const Cliente = require("./database/cliente"); // Configurar o model da aplicaç
 const Endereco = require("./database/endereco");
 
 // Definição de rotas
-app.post("/clientes", (req, res)=> {
-  const { nome, email, telefone } = req.body;
-  console.log(nome, email, telefone);
-  res.json("Recebido")
-})
+app.get("/clientes", async (req, res) => {
+  //SELECT FROM clientes;
+  const listaClientes = await Cliente.findAll();
+  res.json(listaClientes);
+});
+
+//clientes/1,2
+app.get("/clientes/:id", async (req, res) => {
+  const cliente = await Cliente.findOne({
+    where: { id: req.params.id },
+    include: [Endereco],
+  });
+  if(cliente) {
+    res.json(cliente);
+  } else {
+    res.status(404).json({ error: "Usuário não encontrado"});
+  }
+});
+
+app.post("/clientes", async (req, res) => {
+// Coletar os dados do req.body
+  const { nome, email, telefone, endereco } = req.body;
+
+  try {
+    // Dentro de 'novo' estará o objeto criado
+    const novo = await Cliente.create(
+      { nome, email, telefone, endereco },
+      { include: [Endereco] } //Permite inserir cliente e endereço num comento
+      );
+    res.status(201).json(novo)
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Um erro aconteceu" })
+  }
+});
 
 // Escuta de eventos (listen)
 app.listen(3000, () => {
